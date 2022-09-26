501db82895d977ac5fef5f74b15cc3b359cff18e
Montse Viyuela <mprog1920@gmail.com>
2022-09-26 20:07:17 +0200

    Modificados datos de estudiantes e1 y e3

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -11,11 +11,11 @@ public class TestEstudiante
      */
     public static void main(String[] args) {
         System.out.println("Creamos varios objetos Estudiante");
-        Estudiante e1 = new Estudiante("Alberto", 6);
+        Estudiante e1 = new Estudiante("Maite", 9);
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana Isabel", 8);
         e2.printEstudiante();
-        Estudiante e3 = new Estudiante("Juan", 26);
+        Estudiante e3 = new Estudiante("Juan", 6);
         e3.printEstudiante();
 
     }
