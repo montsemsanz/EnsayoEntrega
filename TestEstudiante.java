e9511b3951705b356d3597309ec5128f3f46b652
Montse Viyuela <mprog192@gmail.com>
2022-10-04 12:22:21 +0200

    Modificado nombre de e3 en clase TestEstudiante

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -15,7 +15,7 @@ public class TestEstudiante
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana Isabel", 8);
         e2.printEstudiante();
-        Estudiante e3 = new Estudiante("Elisa", 26);
+        Estudiante e3 = new Estudiante("Maite", 26);
         e3.printEstudiante();
 
     }
